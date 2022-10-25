ac7ac3b03400141b1ac78316c2cbca042c4ebf5b
BenKoehler <ben.koehler@web.de>
2022-10-25 22:16:43 +0200

    add version macro

--- bk.cmake
+++ bk.cmake
@@ -1,3 +1,4 @@
+set(BK_LIBHARU_VERSION "2.3.0")
 function(link_libharu target MOD)
     target_link_libraries(${target} ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/libharu/lib/libharu.so)
     target_include_directories(${target} SYSTEM ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/libharu/include)
